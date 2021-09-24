e27677cf4e9f3fee05eb33f34843d41c359525a8
GiladWrona <90249718+GiladWrona@users.noreply.github.com>
2021-09-24 13:37:26 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var menuKnop = document.querySelector("header nav a img");
+
+function hamburgerMenu() {
+    var menu = document.querySelector("header nav ul");
+    menu.classList.toggle("MenuUit");
+}
+
+menuKnop.addEventListener ("click" , hamburgerMenu);
