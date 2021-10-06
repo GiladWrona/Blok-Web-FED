897498b18fc03cc216e149d3f80651267a15755e
GiladWrona <90249718+GiladWrona@users.noreply.github.com>
2021-10-06 16:55:58 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
 // JavaScript Document
-var menuKnop = document.querySelector("header nav button img");
+var menuKnop = document.querySelector("body > section nav button img");
 
 function hamburgerMenu() {
-    var menu = document.querySelector("header nav ul");
+    var menu = document.querySelector("body > section nav ul");
     menu.classList.toggle("MenuUit");
 }
 
 menuKnop.addEventListener ("click" , hamburgerMenu);
+
+
+
+var loadMoreBtn = document.querySelector("#home main section:nth-of-type(4) > button");
+var items = document.querySelector(".items");
+
+
+function loadMore(){
+    items.classList.toggle("load-more");
+}
+
+loadMoreBtn.addEventListener("click" , loadMore);
